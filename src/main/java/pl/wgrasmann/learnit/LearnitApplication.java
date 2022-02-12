7b946564c9661b9233ee97323f8b92f4c2abd06c
wgrasmann <wgrasmann@gmail.com>
2022-02-12 10:06:45 +0100

    first commit

---
+++ src/main/java/pl/wgrasmann/learnit/LearnitApplication.java
@@ -0,0 +1,13 @@
+package pl.wgrasmann.learnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wgrasmann/learnit/LearnitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wgrasmann.learnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
